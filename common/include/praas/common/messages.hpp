e22eeb9156bef1070d87e114955bf041d6a05e38
Marcin Copik <mcopik@gmail.com>
2022-10-22 04:12:48 +0200

    [common] Update message structure to new project requirements - involve only communication between control plane and processes

--- common/include/praas/common/messages.hpp
+++ common/include/praas/common/messages.hpp
@@ -10,10 +10,11 @@ namespace praas::common {
 
   struct MessageType {
     enum class Type : int16_t {
-      CLIENT = 0,
-      PROCESS = 1,
-      SESSION = 2,
-      SESSION_STATUS = 3
+      PROCESS_CONNECTION = 1,
+      SWAP_CONFIRMATION = 2,
+      INVOCATION_RESPONSE = 3,
+      DATAPLANE_METRICS = 4,
+      PROCESS_CLOSURE = 5
     };
 
     MessageType() = default;
@@ -27,78 +28,88 @@ namespace praas::common {
   };
 
   struct Header {
-    // Connection Headers
 
-    // Client connection - DISABLED
-    // 2 bytes of identifier: 0
-    // 4 bytes of packet size
-    // 16 bytes of process name
-    // 16 bytes of function name
-    // 16 bytes of session id
-    // 54 bytes
+    static constexpr uint16_t NAME_LENGTH = 32;
+
+    // Connection Headers
 
     // Process connection
     // 2 bytes of identifier: 1
-    // 16 bytes of process id
-    // 18 bytes
+    // 32 bytes of process name
+    // 34 bytes
 
-    // Session connection
+    // Swap confirmation
     // 2 bytes of identifier: 2
-    // 16 bytes of session id
+    // 2 bytes
+
+    // Invocation response
+    // 2 bytes of identifier: 3
+    // 32 bytes of invocation id
+    // 8 bytes response size
+    // 42 bytes
+
+    // Data plane metrics
+    // 2 bytes of identifier: 4
+    // 4 bytes of invocations
+    // 4 bytes of computation time
+    // 8 bytes last invocation
     // 18 bytes
 
-    // Process notifying session status
-    // 2 bytes of identifier: 3
-    // 2 bytes of shared memory status
-    // 4 bytes of shared memory size
-    // 16 bytes of session id
+    // Closure of process
+    // 2 bytes of identifier: 5
+    // 2 bytes
 
-    static constexpr uint16_t BUF_SIZE = 24;
+    static constexpr uint16_t BUF_SIZE = 42;
     std::array<int8_t, BUF_SIZE> data;
 
     std::unique_ptr<MessageType> parse();
-    bool is_session_status();
   };
 
-  struct ClientMessage : MessageType {
-    static constexpr uint16_t EXPECTED_LENGTH = 54;
+  struct ProcessConnectionMessage : MessageType {
+    static constexpr uint16_t EXPECTED_LENGTH = 34;
     int8_t* buf;
 
-    ClientMessage(int8_t* buf) : buf(buf) {}
+    ProcessConnectionMessage(int8_t* buf) : buf(buf) {}
 
-    int32_t payload_size();
-    std::string process_name();
-    std::string function_name();
-    std::string session_id();
+    std::string process_name() const;
     Type type() const override;
   };
 
-  struct ProcessMessage : MessageType {
-    int8_t* buf;
+  struct SwapConfirmation : MessageType {
 
-    ProcessMessage(int8_t* buf) : buf(buf) {}
+    SwapConfirmation(int8_t*) {}
 
-    std::string process_id();
     Type type() const override;
   };
 
-  struct SessionMessage : MessageType {
+  struct InvocationResponseMessage : MessageType {
     int8_t* buf;
 
-    SessionMessage(int8_t* buf) : buf(buf) {}
+    InvocationResponseMessage(int8_t* buf) : buf(buf) {}
 
-    std::string session_id();
+    std::string invocation_id() const;
+    int32_t response_size() const;
     Type type() const override;
   };
 
-  struct SessionStatusMessage : MessageType {
+  struct DataPlaneMetricsMessage : MessageType {
     int8_t* buf;
 
-    SessionStatusMessage(int8_t* buf) : buf(buf) {}
+    DataPlaneMetricsMessage(int8_t* buf) : buf(buf) {}
 
-    int32_t memory_size();
-    std::string session_id();
-    int16_t session_status();
+    // 4 bytes of invocations
+    // 4 bytes of computation time
+    // 8 bytes last invocation
+    int32_t invocations() const;
+    int32_t computation_time() const;
+    uint64_t last_invocation_timestamp() const;
+    Type type() const override;
+  };
+
+  struct ClosureMessage : MessageType {
+
+    ClosureMessage(int8_t*) {}
+
     Type type() const override;
   };
 
@@ -115,66 +126,66 @@ namespace praas::common {
     Request(): data() { }
   };
 
-  struct ProcessRequest : Request {
+  //struct ProcessRequest : Request {
 
-    // Process Allocation
-    // 2 bytes identifier
-    // 2 bytes max_sessions
-    // 4 bytes port
-    // 15 bytes IP address
-    // 16 bytes process_id
-    // 39 bytes
+  //  // Process Allocation
+  //  // 2 bytes identifier
+  //  // 2 bytes max_sessions
+  //  // 4 bytes port
+  //  // 15 bytes IP address
+  //  // 16 bytes process_id
+  //  // 39 bytes
 
-    using Request::data;
-    using Request::EXPECTED_LENGTH;
+  //  using Request::data;
+  //  using Request::EXPECTED_LENGTH;
 
-    int16_t max_sessions();
-    int32_t port();
-    std::string ip_address();
-    std::string process_id();
+  //  int16_t max_sessions();
+  //  int32_t port();
+  //  std::string ip_address();
+  //  std::string process_id();
 
-    ssize_t fill(
-        int16_t sessions, int32_t port, std::string ip_address,
-        std::string process_id
-    );
-  };
+  //  ssize_t fill(
+  //      int16_t sessions, int32_t port, std::string ip_address,
+  //      std::string process_id
+  //  );
+  //};
 
-  struct SessionRequest : Request {
+  //struct SessionRequest : Request {
 
-    // Session Allocation
-    // 2 bytes of identifier
-    // 4 bytes max functions
-    // 4 bytes memory size
-    // 16 bytes of session id
-    // 26 bytes
+  //  // Session Allocation
+  //  // 2 bytes of identifier
+  //  // 4 bytes max functions
+  //  // 4 bytes memory size
+  //  // 16 bytes of session id
+  //  // 26 bytes
 
-    using Request::data;
-    using Request::EXPECTED_LENGTH;
+  //  using Request::data;
+  //  using Request::EXPECTED_LENGTH;
 
-    int16_t max_functions();
-    int32_t memory_size();
-    std::string session_id();
+  //  int16_t max_functions();
+  //  int32_t memory_size();
+  //  std::string session_id();
 
-    ssize_t
-    fill(std::string session_id, int32_t max_functions, int32_t memory_size);
-  };
+  //  ssize_t
+  //  fill(std::string session_id, int32_t max_functions, int32_t memory_size);
+  //};
 
-  struct FunctionRequest : Request {
+  //struct FunctionRequest : Request {
 
-    // Session Allocation
-    // 2 bytes of identifier
-    // 4 bytes payload size
-    // 16 bytes of function name
-    // 16 bytes of invocation id
-    // 32 bytes
+  //  // Session Allocation
+  //  // 2 bytes of identifier
+  //  // 4 bytes payload size
+  //  // 16 bytes of function name
+  //  // 16 bytes of invocation id
+  //  // 32 bytes
 
-    using Request::data;
-    using Request::EXPECTED_LENGTH;
+  //  using Request::data;
+  //  using Request::EXPECTED_LENGTH;
 
-    ssize_t fill(
-        std::string function_name, std::string function_id, int32_t payload_size
-    );
-  };
+  //  ssize_t fill(
+  //      std::string function_name, std::string function_id, int32_t payload_size
+  //  );
+  //};
 
 } // namespace praas::common
 
--- common/src/messages.cpp
+++ common/src/messages.cpp
@@ -8,158 +8,143 @@ namespace praas::common {
   std::unique_ptr<MessageType> Header::parse()
   {
     int16_t type = *reinterpret_cast<int16_t*>(data.data());
-    if (type == static_cast<int16_t>(MessageType::Type::CLIENT)) {
-      return std::make_unique<ClientMessage>(data.data() + 2);
-    } else if (type == static_cast<int16_t>(MessageType::Type::PROCESS)) {
-      return std::make_unique<ProcessMessage>(data.data() + 2);
-    } else if (type == static_cast<int16_t>(MessageType::Type::SESSION)) {
-      return std::make_unique<SessionMessage>(data.data() + 2);
-    } else if (type == static_cast<int16_t>(MessageType::Type::SESSION_STATUS)) {
-      return std::make_unique<SessionStatusMessage>(data.data() + 2);
+    if (type == static_cast<int16_t>(MessageType::Type::PROCESS_CONNECTION)) {
+      return std::make_unique<ProcessConnectionMessage>(data.data() + 2);
+    } else if (type == static_cast<int16_t>(MessageType::Type::SWAP_CONFIRMATION)) {
+      return std::make_unique<SwapConfirmation>(data.data() + 2);
+    } else if (type == static_cast<int16_t>(MessageType::Type::INVOCATION_RESPONSE)) {
+      return std::make_unique<InvocationResponseMessage>(data.data() + 2);
+    } else if (type == static_cast<int16_t>(MessageType::Type::DATAPLANE_METRICS)) {
+      return std::make_unique<DataPlaneMetricsMessage>(data.data() + 2);
+    } else if (type == static_cast<int16_t>(MessageType::Type::PROCESS_CLOSURE)) {
+      return std::make_unique<ClosureMessage>(data.data() + 2);
     } else {
       return nullptr;
     }
   }
 
-  int32_t ClientMessage::payload_size()
+  std::string ProcessConnectionMessage::process_name() const
+  {
+    return std::string{reinterpret_cast<char*>(buf + 2), Header::NAME_LENGTH};
+  }
+
+  MessageType::Type ProcessConnectionMessage::type() const
+  {
+    return MessageType::Type::PROCESS_CONNECTION;
+  }
+
+  MessageType::Type SwapConfirmation::type() const
+  {
+    return MessageType::Type::SWAP_CONFIRMATION;
+  }
+
+  std::string InvocationResponseMessage::invocation_id() const
+  {
+    return std::string{reinterpret_cast<char*>(buf), Header::NAME_LENGTH};
+  }
+
+  int32_t InvocationResponseMessage::response_size() const
+  {
+    return *reinterpret_cast<int32_t*>(buf + Header::NAME_LENGTH);
+  }
+
+  MessageType::Type InvocationResponseMessage::type() const
+  {
+    return MessageType::Type::INVOCATION_RESPONSE;
+  }
+
+  int32_t DataPlaneMetricsMessage::invocations() const
   {
     return *reinterpret_cast<int32_t*>(buf);
   }
 
-  std::string ClientMessage::function_name()
+  int32_t DataPlaneMetricsMessage::computation_time() const
   {
-    return std::string{reinterpret_cast<char*>(buf + 20)};
+    return *reinterpret_cast<int32_t*>(buf + 4);
   }
 
-  std::string ClientMessage::process_name()
+  uint64_t DataPlaneMetricsMessage::last_invocation_timestamp() const
   {
-    return std::string{reinterpret_cast<char*>(buf + 4)};
+    return *reinterpret_cast<uint64_t*>(buf + 8);
   }
 
-  std::string ClientMessage::session_id()
+  MessageType::Type DataPlaneMetricsMessage::type() const
   {
-    return std::string{reinterpret_cast<char*>(buf + 36)};
+    return MessageType::Type::DATAPLANE_METRICS;
   }
 
-  MessageType::Type ClientMessage::type() const
+  MessageType::Type ClosureMessage::type() const
   {
-    return MessageType::Type::CLIENT;
+    return MessageType::Type::PROCESS_CLOSURE;
   }
 
-  std::string ProcessMessage::process_id()
-  {
-    // FIXME: compute string length
-    return std::string{reinterpret_cast<char*>(buf), 16};
-  }
+  //int16_t SessionRequest::max_functions()
+  //{
+  //  return *reinterpret_cast<int16_t*>(data.data() + 2);
+  //}
 
-  MessageType::Type ProcessMessage::type() const
-  {
-    return MessageType::Type::PROCESS;
-  }
+  //int32_t SessionRequest::memory_size()
+  //{
+  //  return *reinterpret_cast<int32_t*>(data.data() + 4);
+  //}
 
-  std::string SessionMessage::session_id()
-  {
-    // FIXME: compute string length
-    return std::string{reinterpret_cast<char*>(buf), 16};
-  }
+  //std::string SessionRequest::session_id()
+  //{
+  //  return std::string{reinterpret_cast<char*>(data.data() + 10), 16};
+  //}
 
-  MessageType::Type SessionMessage::type() const
-  {
-    return MessageType::Type::SESSION;
-  }
+  //ssize_t SessionRequest::fill(std::string session_id, int32_t max_functions, int32_t memory_size)
+  //{
+  //  *reinterpret_cast<int16_t*>(data.data()) =
+  //      static_cast<int16_t>(Request::Type::SESSION_ALLOCATION);
+  //  *reinterpret_cast<int32_t*>(data.data() + 2) = max_functions;
+  //  *reinterpret_cast<int32_t*>(data.data() + 6) = memory_size;
+  //  std::strncpy(reinterpret_cast<char*>(data.data() + 10), session_id.data(), 16);
+  //  return EXPECTED_LENGTH;
+  //}
 
-  int32_t SessionStatusMessage::memory_size()
-  {
-    return *reinterpret_cast<int32_t*>(buf);
-  }
+  //int16_t ProcessRequest::max_sessions()
+  //{
+  //  return *reinterpret_cast<int16_t*>(data.data() + 2);
+  //}
 
-  int16_t SessionStatusMessage::session_status()
-  {
-    return *reinterpret_cast<int16_t*>(buf + 4);
-  }
+  //int32_t ProcessRequest::port()
+  //{
+  //  return *reinterpret_cast<int32_t*>(data.data() + 4);
+  //}
 
-  std::string SessionStatusMessage::session_id()
-  {
-    // FIXME: compute string length
-    return std::string{reinterpret_cast<char*>(buf + 6), 16};
-  }
+  //std::string ProcessRequest::ip_address()
+  //{
+  //  return std::string{reinterpret_cast<char*>(data.data() + 8), 15};
+  //}
 
-  MessageType::Type SessionStatusMessage::type() const
-  {
-    return MessageType::Type::SESSION_STATUS;
-  }
+  //std::string ProcessRequest::process_id()
+  //{
+  //  return std::string{reinterpret_cast<char*>(data.data() + 23), 16};
+  //}
 
-  int16_t SessionRequest::max_functions()
-  {
-    return *reinterpret_cast<int16_t*>(data.data() + 2);
-  }
+  //ssize_t ProcessRequest::fill(
+  //    int16_t sessions, int32_t port, std::string ip_address, std::string process_id
+  //)
+  //{
+  //  *reinterpret_cast<int16_t*>(data.data()) =
+  //      static_cast<int16_t>(Request::Type::PROCESS_ALLOCATION);
+  //  *reinterpret_cast<int16_t*>(data.data() + 2) = sessions;
+  //  *reinterpret_cast<int32_t*>(data.data() + 4) = port;
+  //  std::strncpy(reinterpret_cast<char*>(data.data() + 8), ip_address.data(), 15);
+  //  std::strncpy(reinterpret_cast<char*>(data.data() + 23), process_id.data(), 16);
+  //  return EXPECTED_LENGTH;
+  //}
 
-  int32_t SessionRequest::memory_size()
-  {
-    return *reinterpret_cast<int32_t*>(data.data() + 4);
-  }
-
-  std::string SessionRequest::session_id()
-  {
-    return std::string{reinterpret_cast<char*>(data.data() + 10), 16};
-  }
-
-  ssize_t SessionRequest::fill(
-      std::string session_id, int32_t max_functions, int32_t memory_size
-  )
-  {
-    *reinterpret_cast<int16_t*>(data.data()) =
-        static_cast<int16_t>(Request::Type::SESSION_ALLOCATION);
-    *reinterpret_cast<int32_t*>(data.data() + 2) = max_functions;
-    *reinterpret_cast<int32_t*>(data.data() + 6) = memory_size;
-    std::strncpy(reinterpret_cast<char*>(data.data() + 10), session_id.data(), 16);
-    return EXPECTED_LENGTH;
-  }
-
-  int16_t ProcessRequest::max_sessions()
-  {
-    return *reinterpret_cast<int16_t*>(data.data() + 2);
-  }
-
-  int32_t ProcessRequest::port()
-  {
-    return *reinterpret_cast<int32_t*>(data.data() + 4);
-  }
-
-  std::string ProcessRequest::ip_address()
-  {
-    return std::string{reinterpret_cast<char*>(data.data() + 8), 15};
-  }
-
-  std::string ProcessRequest::process_id()
-  {
-    return std::string{reinterpret_cast<char*>(data.data() + 23), 16};
-  }
-
-  ssize_t ProcessRequest::fill(
-      int16_t sessions, int32_t port, std::string ip_address,
-      std::string process_id
-  )
-  {
-    *reinterpret_cast<int16_t*>(data.data()) =
-        static_cast<int16_t>(Request::Type::PROCESS_ALLOCATION);
-    *reinterpret_cast<int16_t*>(data.data() + 2) = sessions;
-    *reinterpret_cast<int32_t*>(data.data() + 4) = port;
-    std::strncpy(reinterpret_cast<char*>(data.data() + 8), ip_address.data(), 15);
-    std::strncpy(reinterpret_cast<char*>(data.data() + 23), process_id.data(), 16);
-    return EXPECTED_LENGTH;
-  }
-
-  ssize_t FunctionRequest::fill(
-      std::string function_name, std::string function_id, int32_t payload_size
-  )
-  {
-    *reinterpret_cast<int16_t*>(data.data()) =
-        static_cast<int16_t>(Request::Type::FUNCTION_INVOCATION);
-    *reinterpret_cast<int16_t*>(data.data() + 2) = payload_size;
-    std::strncpy(reinterpret_cast<char*>(data.data() + 6), function_name.data(), 16);
-    std::strncpy(reinterpret_cast<char*>(data.data() + 22), function_id.data(), 16);
-    return EXPECTED_LENGTH;
-  }
+  //ssize_t
+  //FunctionRequest::fill(std::string function_name, std::string function_id, int32_t payload_size)
+  //{
+  //  *reinterpret_cast<int16_t*>(data.data()) =
+  //      static_cast<int16_t>(Request::Type::FUNCTION_INVOCATION);
+  //  *reinterpret_cast<int16_t*>(data.data() + 2) = payload_size;
+  //  std::strncpy(reinterpret_cast<char*>(data.data() + 6), function_name.data(), 16);
+  //  std::strncpy(reinterpret_cast<char*>(data.data() + 22), function_id.data(), 16);
+  //  return EXPECTED_LENGTH;
+  //}
 
 } // namespace praas::common
