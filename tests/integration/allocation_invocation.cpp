3ac0428d40e7fc0469c4202857295ae35af20815
Marcin Copik <mcopik@gmail.com>
2023-08-06 03:56:27 +0200

    [tests] Add invocation end-to-end test

---
+++ tests/integration/allocation_invocation.cpp
@@ -0,0 +1,64 @@
+
+#include <praas/common/http.hpp>
+#include <praas/control-plane/config.hpp>
+#include <praas/control-plane/server.hpp>
+#include <praas/sdk/praas.hpp>
+
+#include <chrono>
+#include <thread>
+
+#include <boost/iostreams/stream.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/types/string.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+struct Result {
+  std::string msg;
+
+  template <typename Ar>
+  void save(Ar& archive) const
+  {
+    archive(CEREAL_NVP(msg));
+  }
+
+  template <typename Ar>
+  void load(Ar& archive)
+  {
+    archive(CEREAL_NVP(msg));
+  }
+};
+
+class IntegrationLocalInvocation : public ::testing::Test {};
+
+std::string get_output_binary(const char* buffer, size_t size)
+{
+  Result out;
+  boost::iostreams::stream<boost::iostreams::array_source> stream(buffer, size);
+  cereal::BinaryInputArchive archive_in{stream};
+  out.load(archive_in);
+
+  return out.msg;
+}
+
+TEST_F(IntegrationLocalInvocation, Invoke)
+{
+  praas::common::http::HTTPClientFactory::initialize(1);
+  {
+    praas::sdk::PraaS praas{fmt::format("http://127.0.0.1:{}", 8000)};
+
+    ASSERT_TRUE(praas.create_application("test_app", "spcleth/praas-examples:hello-world-cpp"));
+
+    auto proc = praas.create_process("test_app", "test_process", 1, 1024);
+    ASSERT_TRUE(proc.has_value());
+
+    ASSERT_TRUE(proc->connect());
+
+    auto invoc = proc->invoke("hello-world", "invocation-id", nullptr, 0);
+
+    // auto invoc = praas.invoke("test_app", "hello-world", "");
+
+    ASSERT_EQ(invoc.return_code, 0);
+    EXPECT_EQ("Hello, world!", get_output_binary(invoc.payload.get(), invoc.payload_len));
+  }
+}
