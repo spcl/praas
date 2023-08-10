3504da584d0d7c0edf838d7755432ce71a0c0b9a
Marcin Copik <mcopik@gmail.com>
2023-08-10 03:39:30 +0200

    [examples] Move LaTeX dependencies to the example directory

--- examples/latex-editor-python/files.py
+++ examples/latex-editor-python/files.py
@@ -27,10 +27,8 @@ def update_file(invocation, context):
     input = File.from_json(invocation.args[0].str())
     if os.path.splitext(input.file)[1] in ['.pdf', '.png']:
         input.data = base64.b64decode(input.data)
-        print(len(input.data))
 
     path = os.path.join(input.path, input.file)
-    print(path)
     context.state(path, input.data)
 
     out_buf = context.get_output_buffer()
@@ -45,7 +43,10 @@ def get_file(invocation, context):
 
     path = os.path.join(input.path, input.file)
     data = context.state(path)
-    input.data = data.str()
+    if os.path.splitext(input.file)[1] in ['.pdf', '.png']:
+        input.data = base64.b64encode(data.view_readable()).decode()
+    else:
+        input.data = data.str()
 
     out_buf = context.get_output_buffer()
     json.dump(input, pypraas.BufferStringWriter(out_buf), cls=EnhancedJSONEncoder)
---
+++ examples/latex-editor-python/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+apt-get install -y --no-install-recommends latexmk texlive-base texlive-latex-extra texlive-latex-recommended aspell cm-super texlive-plain-generic texlive-fonts-recommended
